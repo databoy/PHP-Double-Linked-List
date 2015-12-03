6678499f5b444d0409637cd3f41adbbfd4da5ee8
Chris Pitchford <chris@databoy.biz>
2015-12-03 10:05:32 -0700

    Added new test

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -5,6 +5,12 @@
  * User: chris
  * Date: 12/3/15
  * Time: 8:12 AM
+ *
+ * TODO: ensure current() throws
+ *  Method must throw an exception if the list is empty.
+    Method must throw an exception if one of the traversal methods (first(), last(), next(), or previous()) has not been called to set a current element.
+    Method must throw an exception if one of the traversal methods (next(), or previous()) has gone beyond the list.
+ *
  */
 class ViewTest extends PHPUnit_Framework_TestCase
 {
